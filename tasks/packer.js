6ea76475fe6a1b81b720fbc61c9e696f152dcf48
artin-phares <artin.phares@gmail.com>
2017-09-27 17:57:45 +0300

    #13 fix initial build of build:watch
    remove callback from webpack() call
    
    if callback exists, then compilation run in place. better start compilation explicitly with compiler.run()
    
    in case of build:watch, compilation run twise (getting compiler, running dev server) and somehow crashes initial build

--- tasks/packer.js
+++ tasks/packer.js
@@ -161,15 +161,7 @@ function pack(opts) {
         resolve = res;
     });
 
-    const compiler = webpack(config,
-        function(err, stats) {
-            if (err) {
-                throw new gutil.PluginError('webpack', err);
-            }
-            gutil.log('[webpack]', stats.toString('minimal'));
-            
-            resolve();
-        });
+    const compiler = webpack(config);
 
     if (opts.watch) {
 
@@ -184,7 +176,7 @@ function pack(opts) {
             proxy: {
                 // proxying all requests (except bundle request)
                 // to prod server (static, api)
-                '*': `http://${opts.server.host}:${opts.server.port}/`
+                '/': `http://${opts.server.host}:${opts.server.port}/`
             },
             
             stats: 'minimal'
@@ -199,6 +191,16 @@ function pack(opts) {
                 `'[webpack-dev-server]' Listening at ` +
                 `${opts.devServer.host}:${opts.devServer.port}`
             ));
+        });
+    } else {
+        
+        compiler.run(function(err, stats) {
+            if (err || stats.hasErrors()) {
+                throw new gutil.PluginError('webpack', err);
+            }
+            gutil.log('[webpack]', stats.toString('minimal'));
+            
+            resolve();
         });
     }
 
